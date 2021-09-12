fd5a07c8b0bd6d438c3d27c44c56d476da249e03
Ognjen Mutavdzic <mutavdzicognjen.1@gmail.com>
2021-09-12 20:04:18 +0200

    preporavka porta i corsa

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 // Add a list of allowed origins.
 // If you have more origins you would like to add, you can add them to the array below.
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
 
 const options: cors.CorsOptions = {
     origin: allowedOrigins
@@ -19,6 +19,6 @@ app.use(cors(options));
 app.use(bodyParser.json());
 app.use("/", ruter);
 
-app.listen(3000, () => {
-    console.log("Node server started running on port 3000");
+app.listen(3001, () => {
+    console.log("Node server started running on port 3001");
 });
