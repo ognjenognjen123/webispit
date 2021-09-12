70542fa8daf74b2e1b123df50b58e6dc2661fbc9
Ognjen Mutavdzic <mutavdzicognjen.1@gmail.com>
2021-09-12 18:12:05 +0200

    dodavanje cors zastite

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import * as bodyParser from "body-parser";
 import {ruter} from "./rute";
-
+import cors from "cors";
 
 const app = express();
+
+
+// Add a list of allowed origins.
+// If you have more origins you would like to add, you can add them to the array below.
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+};
+app.use(cors(options));
+//--
 
 app.use(bodyParser.json());
 app.use("/", ruter);
